a58008d89d63717624f0acb902857235c3cbba27
Elly Loel <hello@ellyloel.com>
2023-03-14 14:59:31 +1100

    ✨ feat: updating styles

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,10 @@
 module.exports = {
 	plugins: [
 		require("postcss-import"),
-		require("postcss-preset-env")({ stage: 0 }),
+		require("postcss-preset-env")({
+			stage: 0,
+			features: { "cascade-layers": false },
+		}),
 		require("cssnano"),
 	],
 };
